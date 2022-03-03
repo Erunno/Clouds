bc5bc984175a79d687a392d254d0d7077ca8f548
Matyáš Brabec <matyas.brabec@profinit.eu>
2022-03-03 17:32:10 +0100

    init

---
+++ lab03/TestStorage/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿using Azure.Storage.Blobs;
+
+var connectionString = "DefaultEndpointsProtocol=https;AccountName=myteststoragewithunique;AccountKey=vlZKLxCpl/SiIxAAnYDrFH/hukTXtZrlOenuLwWwdLYushR1xvmqXDXrGNsotHflRj67T3EVkEp3+AStUBT0OA==;EndpointSuffix=core.windows.net";
+var containerString = "myteststoragewithunique";
+
+var client = new BlobContainerClient(connectionString, containerString);
