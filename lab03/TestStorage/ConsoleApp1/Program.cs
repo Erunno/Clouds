93958cab0e63aaa2dad46972b411575f6468a31e
Matyáš Brabec <matyas.brabec@profinit.eu>
2022-03-03 17:59:07 +0100

    treti lab

--- lab03/TestStorage/ConsoleApp1/Program.cs
+++ lab03/TestStorage/ConsoleApp1/Program.cs
@@ -1,6 +1,12 @@
 ﻿using Azure.Storage.Blobs;
 
 var connectionString = "DefaultEndpointsProtocol=https;AccountName=myteststoragewithunique;AccountKey=vlZKLxCpl/SiIxAAnYDrFH/hukTXtZrlOenuLwWwdLYushR1xvmqXDXrGNsotHflRj67T3EVkEp3+AStUBT0OA==;EndpointSuffix=core.windows.net";
-var containerString = "myteststoragewithunique";
+var containerString = "testcontainer";
 
 var client = new BlobContainerClient(connectionString, containerString);
+var blobs = client.GetBlobs();
+
+foreach (var blob in blobs)
+{
+    Console.WriteLine(blob.Name); //prints hello.txt in my case
+}
